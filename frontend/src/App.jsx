c1b3791022f19e4c90737266275c16589a5fed48
rakhim1502 <uzbrm95@gmail.com>
2026-06-24 19:29:38 +0500

    Comment out Login route in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import EquipmentList from './pages/Equipment/List';
 import EquipmentDetails from './pages/Equipment/Details';
 import RentalCreate from './pages/Rental/Create';
@@ -45,7 +45,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
-                <Route path="/login" element={<Login />} />
+                {/* <Route path="/login" element={<Login />} /> */}
                 <Route path="/equipment" element={<EquipmentList />} />
                 <Route path="/equipment/:id" element={<EquipmentDetails />} />
 
